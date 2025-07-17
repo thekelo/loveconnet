2b72b9dab9ad740ae2493c03e21767fc3131847e
thekelo <bestshowtv6@gmail.com>
2025-07-17 10:23:25 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+// js/firebase.js
 
+// Replace with your actual config
+const firebaseConfig = {
+  apiKey: "AIzaSyCyR7m47iJY_9TLgR93pKzNdPV9VFWeVak",
+  authDomain: "findlove-dating-app-5765d.firebaseapp.com",
+  projectId: "findlove-dating-app-5765d",
+  storageBucket: "findlove-dating-app-5765d.firebasestorage.app",
+  messagingSenderId: "109134771821",
+  appId: "1:109134771821:web:a2e75e5c426c8046ecf164"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
